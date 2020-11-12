b0d406860a463bf5543a30fa0e63b522711a3b87
Ryan DV <rdevilla@uwaterloo.ca>
2020-11-12 11:09:06 -0500

    Declare procedures extern

--- swabang.h
+++ swabang.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 
-void ws2812b_color(uint8_t red, uint8_t green, uint8_t blue);
-void ws2812b_reset();
+extern void ws2812b_color(uint8_t red, uint8_t green, uint8_t blue);
+extern void ws2812b_reset();
